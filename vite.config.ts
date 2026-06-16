7df0fe57c6e1d79a7c1a86b75a2ef87b98e42f9a
engnaser <114346865+engnaser@users.noreply.github.com>
2026-06-17 02:31:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { tanstackStartNetlify } from "@netlify/vite-plugin-tanstack-start";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   vite: {
-    plugins: [tanstackStartNetlify()],
+    plugins: [netlify()],
   },
   tanstackStart: {
     server: { entry: "server" },
